7b1c70e988e377dfd29df236394622740e6d83a6
Rana718 <ranadolui718@gmail.com>
2024-09-08 18:35:26 +0530

    fix the issue

--- backend/GreenGrocer/api/views.py
+++ backend/GreenGrocer/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SigninSerializer, SignupSerializer
+from .auth import signups, signins
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup(request):
+    serializer = SignupSerializer(data=request.data)
+    if serializer.is_valid():
+        return signups(serializer)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signin(request):
+    serializer = SigninSerializer(data=request.data)
+    if serializer.is_valid():
+        return signins(serializer)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
